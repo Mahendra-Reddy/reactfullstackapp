c08d43413a02c5c6d203a485984191414c1fb140
Mahendra <mahendra.b@isteer.in>
2018-03-14 09:27:28 +0530

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,14 @@
-if(process.env.NODE_ENV === "production"){
-  module.exports = require('./prod')
-  const keys = require('./prod');
-}else{
-    module.exports = require('./dev')
-}
+// if(process.env.NODE_ENV === "production"){
+//   module.exports = require('./prod')
+//   const keys = require('./prod');
+// }else{
+//     module.exports = require('./dev')
+// }
+module.exports = {
+  "googleClientID" : "908169808618-19k3o4g7c70hdbgrn2ln2qi07drr4uru.apps.googleusercontent.com",
+  "googleClientSecret" : "x8c8ZYDiGnxBsMgRgkobDWFq",
+  "mongoURI" : "mongodb://emaily:password@ds263948.mlab.com:63948/emaily",
+  "cookieKeys":"Hello world",
+  "facebookClientID":"364263190720417",
+  "facebookClientSecret":"8c8de30db238d2bc10d87ca1ddfc5559" 
+ }
