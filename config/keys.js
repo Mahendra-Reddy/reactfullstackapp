63ceabf616e8b452607179c60c8168f5556c6ed0
Mahendra <mahendra.b@isteer.in>
2018-03-14 09:14:50 +0530

    testing

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
-if(process.env.NODE_ENV==="production"){
+if(process.env.NODE_ENV === "production"){
   module.exports = require('./prod')
+  const keys = require('./prod');
+  console.log(keys.googleClientID)
 }else{
     module.exports = require('./dev')
 }
