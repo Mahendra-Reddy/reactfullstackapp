1b9b6e848f789938fe23c56550b0f6a677075afc
Mahendra <mahendra.b@isteer.in>
2018-03-14 09:40:16 +0530

    keys testing

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,13 @@
-// if(process.env.NODE_ENV === "production"){
-//   module.exports = require('./prod')
-//   const keys = require('./prod');
-// }else{
-//     module.exports = require('./dev')
-// }
-module.exports = {
-  "googleClientID" : "948757398937-6rq2k6qmajgf7gcvaifiog9m8s4gkrbh.apps.googleusercontent.com",
-  "googleClientSecret" : "KsCUZKQj6mrXKWcwKlVD_Lj8",
-  "mongoURI" : "mongodb://mahi_prod:password@ds213239.mlab.com:13239/mahi-emaily-prod",
-  "cookieKeys":"Hello world",
-  "facebookClientID":"364263190720417",
-  "facebookClientSecret":"8c8de30db238d2bc10d87ca1ddfc5559" 
- }
+if(process.env.NODE_ENV === "production"){
+  module.exports = require('./prod')
+}else{
+    module.exports = require('./dev')
+}
+// module.exports = {
+//   "googleClientID" : "948757398937-6rq2k6qmajgf7gcvaifiog9m8s4gkrbh.apps.googleusercontent.com",
+//   "googleClientSecret" : "KsCUZKQj6mrXKWcwKlVD_Lj8",
+//   "mongoURI" : "mongodb://mahi_prod:password@ds213239.mlab.com:13239/mahi-emaily-prod",
+//   "cookieKeys":"Hello world",
+//   "facebookClientID":"364263190720417",
+//   "facebookClientSecret":"8c8de30db238d2bc10d87ca1ddfc5559" 
+//  }
